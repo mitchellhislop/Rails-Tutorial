971be9a586c9b2455010febc10752f4c2050639e
Mitchell Hislop <mitch@mitchellhislop.com>
2010-12-30 21:49:31 -0600

    deploying the demo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Tutorialapp1::Application.routes.draw do
+  resources :microposts
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
